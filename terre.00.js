e9e4b31bf8d7ad777a353c88376c05c5b76f5199
Anh Pati <romain-felicianne@hotmail.fr>
2023-07-12 13:40:33 +0200

    Alphabet
    Script qui affiche une chaîne de caractères listant l'alphabet depuis un code ASCII.

--- terre.00.js
+++ terre.00.js
@@ -1,10 +1,12 @@
 const alphabet = () => {
     let letter = 97
     let alphabet = []
+
     while (letter < 123) {
         alphabet.push(String.fromCharCode(letter))
         letter++
     }
+
     return console.log(alphabet.join(''))
 }
 
